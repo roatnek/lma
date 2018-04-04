0505037c76642267a432d4f29da73251b2fade64
Kentaro Tanaka <tnkktr@gmail.com>
2018-04-04 16:26:32 +0900

    PostCSSからSassに乗り換え

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   plugins: [
-    require('postcss-import')(),
-    // require('postcss-custom-properties')(),
-    require('postcss-nested')(),
     require('autoprefixer')({
       browsers: ['last 2 versions', 'ie 11']
     })
